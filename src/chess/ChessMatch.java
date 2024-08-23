f96b587daba678018d7c4d21e25533f254a2f760
Leonardo Angelo <leoangelo175@gmail.com>
2024-08-23 15:22:00 -0300

    Handling exceptions and clearing screen && Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if (!board.piece(position).isthereAnyPossibleMove()) {
+			throw new ChessException("Trere is no possible moves for the chosen pieces");
+		}
 	}
 	private void placeNewPiece (char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
